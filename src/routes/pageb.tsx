9b69fdd56da7ebc615aa75c0c580eb4aadcb966d
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-17 22:25:04 -0700

    more resources links

--- src/routes/pageb.tsx
+++ src/routes/pageb.tsx
@@ -9,6 +9,9 @@ function PageB() {
     <div style={{ padding: '2rem' }}>
         <h1>this page is for practical things</h1>
         <p>the exact step by step to building projects directly</p>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Cheatsheet">html site 2</a>
+        <a href="https://www.w3schools.com/html/default.asp">html resource site 2</a>
+        <a href="https://nextjs.org/docs">next.js docs</a>
     </div>
   )
 }
