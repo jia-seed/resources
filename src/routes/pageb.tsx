0fdae62ff067f2b294e9e61573a7d989defef15e
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-18 11:41:56 -0700

    vscode info

--- src/routes/pageb.tsx
+++ src/routes/pageb.tsx
@@ -15,7 +15,10 @@ function PageB() {
             <li>- <a href="https://nextjs.org/docs">next.js docs</a></li>
         </ul>
         <h2>using vscode</h2>
-        <p>1. sometimes the code is correct but there are squiggles. just cmd+shift+p and type in typescript: restart ts server. or idk i just quit vscode and open it again and then it's fine. this happens because of a stale ttsserver cache where it's in a confused state after u edit many files.</p>
+        <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <li>1. sometimes the code is correct but there are squiggles. just cmd+shift+p and type in typescript: restart ts server. or idk i just quit vscode and open it again and then it's fine. this happens because of a stale ttsserver cache where it's in a confused state after u edit many files.</li>
+          <li>2. multicursor editing for when you're making n identical edits in n places simultaneously: good shortcuts are (cmd+shift+l, to highlight the n appearance of the thing) and (holding option and clicking every place you want to start adding something, then doing cmd+right arrow and typing what you want at the end. for example, putting several phrases into a list) </li>
+        </ul>
     </div>
   )
 }
