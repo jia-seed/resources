ba5286c284d6b492d86bde7428cf7562eb0d9dd2
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-17 19:49:47 -0700

    paragraphathon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,6 +13,7 @@ function Home() {
   return (
     <div style={{ padding: '2rem' }}>
     <h1>technical stuff</h1>
+    <p>so i made this for my awesome friend entirely from scratch without any coding agents and minimal copy and paste using tan stack start. down in ohio swag like ohio type of thing.</p>
     <Link to="/pagea">conceptual stuff</Link>
     </div>
   )
