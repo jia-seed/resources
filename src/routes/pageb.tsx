2d19401714daf0b6ef0bf98bc70337b66f399473
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-17 22:31:42 -0700

    more spacing

--- src/routes/pageb.tsx
+++ src/routes/pageb.tsx
@@ -10,7 +10,9 @@ function PageB() {
         <h1>this page is for practical things</h1>
         <p>the exact step by step to building projects directly</p>
         <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Cheatsheet">html site 2</a>
-        <a href="https://www.w3schools.com/html/default.asp">html resource site 2</a>
+        <p></p>
+        <a >href="https://www.w3schools.com/html/default.asp">html resource site 2</a>
+        <p></p>
         <a href="https://nextjs.org/docs">next.js docs</a>
     </div>
   )
