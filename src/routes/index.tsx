47bc0a15d416608e438ef460c8272ed5ef325f21
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-17 20:06:11 -0700

    more info, important link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,8 +13,10 @@ function Home() {
   return (
     <div style={{ padding: '2rem' }}>
     <h1>technical stuff</h1>
-    <p>so i made this for my awesome friend entirely from scratch without any coding agents and minimal copy and paste using tan stack start. down in ohio swag like ohio type of thing.</p>
+    <p>so i made this for my awesome friend entirely from scratch without any coding agents and minimal copy and paste using tan stack start. in addition all the contents on the site are also handwritten without ai which means the grammar will be egregious. apologies in advance. down in ohio swag like ohio type of thing.</p>
     <Link to="/pagea">conceptual stuff</Link>
+    <p></p>
+    <a href="https://youtu.be/GfqDIcrhqxM?si=urAhGNyo56dAGgpS">EXTREMELY IMPORTANT CONCEPTS MUST READ</a>
     </div>
   )
 }
