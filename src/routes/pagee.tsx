c3594825307173be76c19ce8c0923dd9041dce29
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-18 16:56:20 -0700

    20 seconds

--- src/routes/pagee.tsx
+++ src/routes/pagee.tsx
@@ -8,6 +8,10 @@ function PageE() {
     return (
         <div style={{ padding: '2rem' }}>
             <h1>security</h1>
+            <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+              <li>rls supabase </li>
+              <li>env variables</li>
+            </ul>
         </div>
   )
 }
