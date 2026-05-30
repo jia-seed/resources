450bb171f75444b31c010c6b69a5eaff1c30a369
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-29 19:14:03 -0700

    codelingo

--- src/routes/pagef.tsx
+++ src/routes/pagef.tsx
@@ -8,6 +8,7 @@ function Leetcode() {
   return (
         <div style={{ padding: '0 4rem 4rem' }}>
             <h1>leetcode</h1>
+            <p>potentially a good resource my friend built codelingo.pro</p>
             <h2>contains duplicate</h2>
             <p>Given an integer array <mark>nums</mark>, return true if any value appears more than once in the array, otherwise return false.</p>
             <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
