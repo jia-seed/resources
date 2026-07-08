7d3dafab67618b6163accafa3603167582b78769
Audrey Chen <Audgeviolin07@gmail.com>
2026-07-08 15:33:27 -0700

    cube

--- src/routes/cube.tsx
+++ src/routes/cube.tsx
@@ -16,7 +16,7 @@ function Cube() {
         however i didn't enjoy it for long because it was time to learn a new method: 2 look oll and pll on 4/30, day 19. it was a big work week and it took a while to integrate/memorize. but then i was rewarded with a pr of 28.94 seconds on 5/10, 29 days in (so i broke sub 30 seconds in under 30 days!).
       </p>
       
-      <p>on day 33 i met a 7 second cuber at vivarium, then told him to coach me at noisebridge the next day. he taught me to start the cross on the bottom and have no cube rotations for f2l. i think i had gotten a 25 second pr after midnight of that day before i met with him. 5/16 i immediately got my dopamine hit with the new bottom cross 1 orientation method working on 5/16 and got 24 seconds.
+      <p>on day 33 i met a cuber guy who solves it in 7 seconds at vivarium (who is now an amazing friend), then told him to school me at noisebridge the next day. he taught me to start the cross on the bottom and have no cube rotations for f2l. i think i had gotten a 25 second pr after midnight of that day before i met with him. 5/16 i immediately got my dopamine hit with the new bottom cross 1 orientation method working on 5/16 and got 24 seconds.
         now it's 5/18 and i woke up in the morning and solved it in 23.52 seconds. i'm currently learning full oll and pll + xcross. ALSO NO I GOT 20.87 sec at 4:44pm pst SAME DAY. enjoy the resources!</p>
       
       <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
